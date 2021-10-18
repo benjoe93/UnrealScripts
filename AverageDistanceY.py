f36d30193d5560559e44618fb462d3cfa5e66f68
benjoe93 <blazerbence@gmail.com>
2021-10-18 17:34:15 +0100

    Update AverageDistanceY.py

--- AverageDistanceY.py
+++ AverageDistanceY.py
@@ -4,30 +4,18 @@ import unreal
 class Global(unreal.GlobalEditorUtilityBase):
     pass
 
-# variables
 selected_actors = Global().get_selection_set()
 
-min = 0
-max = 0
+min = selected_actors[0].get_actor_location().y
+max = selected_actors[-1].get_actor_location().y
 i = 0
-
-# logic
-# find min and max position 
-for actor in selected_actors:
-    actor_loc = actor.get_actor_location().y
-    if actor_loc < min:
-        min = actor_loc
-
-    if actor_loc > max:
-        max = actor_loc
 
 dist = abs(min-max)/(len(selected_actors)-1)
 
-# move actors
-for actor in selected_actors:
-    current_loc = actor.get_actor_location()
-    new_loc_y = min+(dist*i)
-    
+for i in range(1, (len(selected_actors)-1)):
+    current_loc = selected_actors[i].get_actor_location()
+    new_loc_y= min+(dist*i)
     new_loc = (current_loc.x, new_loc_y, current_loc.z)
-    actor.set_actor_location(new_loc, False, False)
-    i += 1
+    
+    selected_actors[i].set_actor_location(new_loc, False, False)
+    unreal.log(selected_actors[i].get_fname())
