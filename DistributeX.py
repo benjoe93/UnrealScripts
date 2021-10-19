5de226990c2aab9d8d2e1abb91fb6c205a37daf9
benjoe93 <blazerbence@gmail.com>
2021-10-19 09:26:12 +0100

    Fixed direction issue, added Undo capability

--- DistributeX.py
+++ DistributeX.py
@@ -1,21 +1,37 @@
 import unreal
 
+# instances
 @unreal.uclass()
 class Global(unreal.GlobalEditorUtilityBase):
     pass
 
-selected_actors = Global().get_selection_set()
+sys_lib = unreal.SystemLibrary()
 
+# varables
+selected_actors = Global().get_selection_set()
 min = selected_actors[0].get_actor_location().x
 max = selected_actors[-1].get_actor_location().x
+dist = abs(min-max)/(len(selected_actors)-1)
 i = 0
 
-dist = abs(min-max)/(len(selected_actors)-1)
+# begin Undo recording
+sys_lib.begin_transaction("Py_util", "Distribute X", None)
 
+# loop through actors
 for i in range(1, (len(selected_actors)-1)):
+    # update undo
+    sys_lib.transact_object(selected_actors[i])
+    
     current_loc = selected_actors[i].get_actor_location()
-    new_loc_x= min+(dist*i)
+    #check the direction of movement
+    if min < max:
+        new_loc_x= min+(dist*i)
+    else:
+        new_loc_x= min-(dist*i)
     new_loc = (new_loc_x, current_loc.y, current_loc.z)
     
     selected_actors[i].set_actor_location(new_loc, False, False)
     unreal.log(selected_actors[i].get_fname())
+
+# close undo
+sys_lib.end_transaction()
