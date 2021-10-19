33849fc0e288bc6229c31cc69e0dde7183fd186b
benjoe93 <blazerbence@gmail.com>
2021-10-19 22:34:54 +0100

    Added Undo capability and refactor

--- AlignZ.py
+++ AlignZ.py
@@ -1,18 +1,30 @@
 import unreal
 
+# instances
 @unreal.uclass()
 class Global(unreal.GlobalEditorUtilityBase):
     pass
 
-selected_actors = Global().get_selection_set()
+sys_lib = unreal.SystemLibrary()
 
+# variables
+selected_actors = Global().get_selection_set()
 align_to = selected_actors[0].get_actor_location().z
 i = 0
 
+# begin Undo recording
+sys_lib.begin_transaction("Py_util", "Align Z", None)
+
+# loop through actors
 for actor in selected_actors:
+    # update undo
+    sys_lib.transact_object(actor)
+    
     current_loc = actor.get_actor_location()
     new_loc = (current_loc.x, current_loc.y, align_to)
-    
     actor.set_actor_location(new_loc, False, False)
     i += i
-    unreal.log("{} actors aligned!".format(i))
+
+# close undo
+sys_lib.end_transaction()
+unreal.log("{} actors aligned!".format(i))
